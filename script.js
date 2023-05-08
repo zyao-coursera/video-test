37817199c5d8d0944042a3ee3f977ef38dbee6f8
Zhuo Yao <zyao@coursera.org>
2023-05-08 13:47:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const video = document.getElementById("my-video");
-const customSeekbar = document.getElementById("custom-seekbar");
 const highlightedTimestamp = document.getElementById("highlighted-timestamp");
 const timestampToHighlight = 60; // Set the timestamp in seconds you want to highlight
+
+document.body.style.position = "relative"; // The absolute positioned highlight will be positioned with respect to the body.
 
 video.addEventListener("timeupdate", () => {
   const currentTime = video.currentTime;
   const duration = video.duration;
   const progressPercentage = (currentTime / duration) * 100;
-  
-  customSeekbar.style.width = progressPercentage + "%";
-  
+
   const highlightPosition = (timestampToHighlight / duration) * 100;
-  highlightedTimestamp.style.left = highlightPosition + "%";
+  highlightedTimestamp.style.left = `calc(${highlightPosition}% - 1px)`; // Offset by half the highlight width.
 
   if (currentTime >= timestampToHighlight) {
     highlightedTimestamp.style.backgroundColor = "#f00";
@@ -20,13 +19,8 @@ video.addEventListener("timeupdate", () => {
   }
 });
 
-// Update the video timestamp when clicking on the custom progress bar
-document.getElementById("custom-progressbar").addEventListener("click", (event) => {
-  const progressBarRect = event.currentTarget.getBoundingClientRect();
-  const clickPosition = event.clientX - progressBarRect.left;
-  const progressBarWidth = progressBarRect.width;
-  const clickPercentage = clickPosition / progressBarWidth;
-  const newTime = video.duration * clickPercentage;
-  
-  video.currentTime = newTime;
+video.addEventListener("loadedmetadata", () => {
+  const videoRect = video.getBoundingClientRect(); 
+  const progressBarRect = videoRect.y + videoRect.height - 5; // Adjust 5 pixels from bottom
+  highlightedTimestamp.style.top = `${progressBarRect}px`; 
 });
