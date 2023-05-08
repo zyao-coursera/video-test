c921ea19857c50d02f485055a98859e7a4e9137d
Zhuo Yao <zyao@coursera.org>
2023-05-08 13:59:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const video = document.getElementById("my-video");
+const customProgressbarWrapper = document.getElementById("custom-progressbar-wrapper");
+const customProgressbar = document.getElementById("custom-progressbar");
 const highlightedTimestamp = document.getElementById("highlighted-timestamp");
 const timestampToHighlight = 60; // Set the timestamp in seconds you want to highlight
 
-document.body.style.position = "relative"; // The absolute positioned highlight will be positioned with respect to the body.
+// Position the custom progress bar wrapper
+video.addEventListener("loadedmetadata", () => {
+  const videoRect = video.getBoundingClientRect();
+  const progressBarTop = videoRect.y + videoRect.height - 5; // Adjust 5 pixels from bottom
+  customProgressbarWrapper.style.top = `${progressBarTop}px`;
+});
 
+// Handle video time updates
 video.addEventListener("timeupdate", () => {
   const currentTime = video.currentTime;
   const duration = video.duration;
   const progressPercentage = (currentTime / duration) * 100;
 
+  // Update custom progress bar width
+  customProgressbar.style.width = progressPercentage + "%";
+
   const highlightPosition = (timestampToHighlight / duration) * 100;
-  highlightedTimestamp.style.left = `calc(${highlightPosition}% - 1px)`; // Offset by half the highlight width.
+  highlightedTimestamp.style.left = highlightPosition + "%";
 
   if (currentTime >= timestampToHighlight) {
     highlightedTimestamp.style.backgroundColor = "#f00";
@@ -19,8 +30,14 @@ video.addEventListener("timeupdate", () => {
   }
 });
 
-video.addEventListener("loadedmetadata", () => {
-  const videoRect = video.getBoundingClientRect(); 
-  const progressBarRect = videoRect.y + videoRect.height - 5; // Adjust 5 pixels from bottom
-  highlightedTimestamp.style.top = `${progressBarRect}px`; 
+// Enable seeking in the custom progress bar
+customProgressbarWrapper.addEventListener("click", (event) => {
+  event.stopPropagation(); // Prevent event from reaching the video element.
+
+  const wrapperRect = customProgressbarWrapper.getBoundingClientRect();
+  const clickedPosition = event.clientX - wrapperRect.left;
+  const clickedPercentage = clickedPosition / wrapperRect.width;
+  const newTime = video.duration * clickedPercentage;
+
+  video.currentTime = newTime;
 });
